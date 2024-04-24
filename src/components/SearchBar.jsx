ba9e8fb6aaf931a8f69803a0c146c690c80446da
angelProg47 <angelmimikyufav@gmail.com>
2024-04-24 13:50:26 -0700

    al fin termine lo nuevo ahora ya esta aqui

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,33 +1,32 @@
-import { useState } from "react"
+import { useState } from "react";
+import "./SearchBar.css"
 
-function SearchBar ({ onSubmit }){
-    const [term, setTerm] = useState('')
+function SearchBar({onSubmit}){
+    const [term, setTerm,] = useState("");
 
     const handleFormSubmit = (event) => {
         event.preventDefault()
-        console.log('Necesito decirle al componente padre sobre los datos')
+        console.log('Necesito decirle al componente padre')
         onSubmit(term)
     }
-
     const handleChange = (event) => {
         console.log(event.target.value)
         setTerm(event.target.value)
     }
-
     const handleClick = () => {
         onSubmit(term)
     }
 
     return(
-        <div>
-            <h1>Search Bar </h1>
-            <form onSubmit={handleFormSubmit}>
+        <div className="search-bar">
+            <h1>Search Bar</h1>
+            <form onSubmit={handleFormSubmit} className="search-bar-form">
                 Confirma tu busqueda: {term}
+                <br />
                 <input onChange={handleChange} value={term}/>
             </form>
-            <button onClick={handleClick}>Click Me! </button>
-            </div>
+            <button onClick={handleClick}>Click Me</button>
+        </div>
     )
 }
-
 export default SearchBar
--- src/components/imageList.jsx
+++ src/components/imageList.jsx
@@ -1,9 +1,16 @@
-function ImageList(){
+import ImageShow from "./ImageShow"
+import './ImageList.css'
+
+function ImagesList({images}){
+
+    const renderImage = images.map((image) =>{
+        return <ImageShow key={image.id} image={image}/>
+    })
+
     return(
-        <div>
-            <h1>Images: </h1>
+        <div className="image-list">
+            <h1>{renderImage}</h1>
         </div>
     )
 }
-
 export default ImageList
